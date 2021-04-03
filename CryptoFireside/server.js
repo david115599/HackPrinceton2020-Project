c9d1fb4a38143bd39ae8ac311e47cb5e62248c45
unknown <david.bershadsky2@gmail.com>
2021-04-02 23:30:13 -0400

    asd
    sda

--- CryptoFireside/server.js
+++ CryptoFireside/server.js
@@ -34,21 +34,6 @@ app.get('/', function(request, response){
 
 app.post('/tokensignin', function(request, body){
 console.log(request.body.id_token);
-
-  // async function verify() {
-  //   const ticket = await client.verifyIdToken({
-  //       idToken: token,
-  //       audience: CLIENT_ID,  // Specify the CLIENT_ID of the app that accesses the backend
-  //       // Or, if multiple clients access the backend:
-  //       //[CLIENT_ID_1, CLIENT_ID_2, CLIENT_ID_3]
-  //   });
-  //   const payload = ticket.getPayload();
-  //   const userid = payload['sub'];
-  //   // If request specified a G Suite domain:
-  //   // const domain = payload['hd'];
-  // }
-  // verify().catch(console.error);
-  
 });
 
 
