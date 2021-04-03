88a048270a580f867104a10e7da4a14916940e55
david115599 <david.bershadsky2@gmail.com>
2021-04-02 20:18:17 -0400

    Initail Commit
    Has A Basic Template for testing backend as well as some basic functionaility I quickly threw together

---
+++ CryptoFireside/public/scripts/scripts.js
@@ -0,0 +1,75 @@
+var toggled = false;
+
+let openBtn = document.querySelector(".openSideNav");
+openBtn.addEventListener("click", () => {
+  toggled = !toggled;
+  if (toggled) {
+    showNav();
+  }
+  if (!toggled) {
+    hideNav();
+  }
+});
+
+function showNav() {
+  document.querySelector(".sideNav").style.width = "300px";
+  document.querySelector('.main-content').style.marginLeft = "300px";
+}
+
+function hideNav() {
+  document.querySelector(".sideNav").style.width = "0";
+  document.querySelector('.main-content').style.marginLeft = "0px";
+}
+
+$(document).ready(function () {
+  $("#my_button").click(function (event) {
+
+    var apiKey = 'wvz6nlmPtKUxSUTEDqeDaJpO1Wkv8jC6zpEQZups';
+    var requestURL = 'https://api.propublica.org/congress/v1/bills/search.json?query=sanders';
+
+    $.ajax({
+      url: requestURL,
+      type: "GET",
+      dataType: 'json',
+      headers: {
+        'X-API-Key': apiKey
+      }
+    }).done(function (data) {
+      var content;
+      for (var i = 0; i < data.results[0].bills.length; i++) {
+        var bill = data.results[0].bills[i];
+        content += '<div class="card">'
+        content += '<div class="card-header" id="heading' + i + '">';
+        content += '<h2 class="mb-0">';
+        content += '<button class="btn btn-link" type="button" data-toggle="collapse" data-target="#collapse' + i + '" aria-expanded="false" aria-controls="collapse' + i + '">';
+        content += "" + bill.short_title + "";
+        content += '</button>';
+        content += '</h2></div>  <div id="collapse' + i + '" class="collapse" aria-labelledby="heading' + i + '" data-parent="#accordionExample"> <div class="card-body">  <div id="bills">';
+        content += "" + bill.title + ""
+        content += '</div></div></div></div>';
+      }
+      $('#bills').html(content);
+      console.log(data)
+    });
+  });
+});
+
+
+const selectElement = document.querySelector('#corrup');
+
+selectElement.addEventListener('change', (event) => {
+  var val = $("#corrup").attr("value");
+  if (val == 0) {
+    var content = " ";
+    for (var i = 116; i >= 80; i--) {
+      content += '<option value= ' + i + ' >' + i + ' </option>'
+    }
+    $('#years').html(content);
+  } else {
+    var content = " ";
+    for (var i = 116; i >= 102; i--) {
+      content += '<option value= ' + i + ' >' + i + ' </option>'
+    }
+    $('#years').html(content);
+  }
+});
---
+++ CryptoFireside/server.js
@@ -0,0 +1,82 @@
+var express = require('express');
+var fs = require('fs');
+var favicon = require('serve-favicon');
+var app = express();
+var methodOverride = require('method-override');
+const apirequest = require('request');
+let rawvideos = fs.readFileSync('videos.json');
+let videos = JSON.parse(rawvideos);
+
+
+app.use(methodOverride('_method'));
+app.use(express.urlencoded());
+
+app.use(express.static('public'));
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+app.use(favicon(__dirname + '/public/images/logo.png'));
+
+var port = process.env.PORT || 3000; //||8000
+app.listen(port, function(){
+  console.log('Easy server listening for requests on port '+ port+'!');
+});
+
+app.get('/', function(request, response){
+  response.status(200);
+  response.setHeader('Content-Type', 'text/html')
+  response.render('index',{feedback:videos.videos});
+//console.log(videos.videos);
+});
+
+app.post('/view', function(request, responsea, body){
+  var options = {
+    url: 'https://api.propublica.org/congress/v1/'+request.body.year+'/senate/members.json',
+    json: true,
+    headers: {
+      "X-API-Key": "wvz6nlmPtKUxSUTEDqeDaJpO1Wkv8jC6zpEQZups"
+    }
+  }
+  var options2 = {
+    url: 'https://api.propublica.org/congress/v1/'+request.body.year+'/house/members.json',
+    json: true,
+    headers: {
+      "X-API-Key": "wvz6nlmPtKUxSUTEDqeDaJpO1Wkv8jC6zpEQZups"
+    }
+  }
+  if (request.body.sh == "1") {
+
+    apirequest.get(options2, function(error, response, body) {
+      console.error('error:', error); // Print the error if one occurred
+      console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+      //console.log('body:', body.results[0].members); // Print the HTML for the Google homepage.
+      responsea.status(200);
+      responsea.setHeader('Content-Type', 'text/html')
+      responsea.render('politicians',{feedback:body.results[0]});
+
+    });
+  }
+  else if(request.body.sh == "0") {
+
+  apirequest.get(options, function(error, response, body) {
+    console.error('error:', error); // Print the error if one occurred
+    console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+    //console.log('body:', body.results[0].members); // Print the HTML for the Google homepage.
+    responsea.status(200);
+    responsea.setHeader('Content-Type', 'text/html')
+    responsea.render('politicians',{feedback:body.results[0]});
+  });}
+
+
+});
+
+app.get('/logout', function(request, response){
+  response.status(200);
+  response.setHeader('Content-Type', 'text/html')
+  response.render('index',{feedback:""});
+});
+
+app.get('/about', function(request, response){
+  response.status(200);
+  response.setHeader('Content-Type', 'text/html')
+  response.render('about',{feedback:""});
+});
